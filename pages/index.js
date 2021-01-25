5fe2e4aed912ef598c16ef8a9958c9e50beefba2
Airton Oliveira <airtonoliveira4@hotmail.com>
2021-01-25 14:50:32 -0300

    codigo da primera aula da imersao React

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgrounImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+              <h1>Titulo do quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Titulo do quiz</h1>
+
+            <p>Lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="github.com/zero1code"/>
+    </QuizBackground>
+  );
 }
---
+++ src/components/Widget/index.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4caf50;
+  background-color: #1c1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1%;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({theme}) => theme.colors.primary};
+
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+export default Widget;
